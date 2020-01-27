e8bb7fef511c75b2583da11f4c449b2ee97296be
Cody Jones <codymjones@ymail.com>
2020-01-27 12:52:36 -0800

    Created blank react app.

---
+++ src/LoanCalculator.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LoanCalculator() {
+  return (
+    <div>Hello</div>
+  );
+}
+
+export default LoanCalculator;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoanCalculator from './LoanCalculator';
+
+ReactDOM.render(
+    <LoanCalculator />,
+    document.getElementById('root')
+);
