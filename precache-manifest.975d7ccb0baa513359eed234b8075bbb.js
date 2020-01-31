d2047eb1607cae0657e0cd640354dce4741f4d4a
cucucachu <codymjones@ymail.com>
2020-01-31 10:16:19 -0800

    Updates

---
+++ precache-manifest.975d7ccb0baa513359eed234b8075bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06fd9bd2780d9d2f764d54f06a0e4ee",
+    "url": "/LoanCalculator/index.html"
+  },
+  {
+    "revision": "baafbeeb47f0f2f7e7d4",
+    "url": "/LoanCalculator/static/js/2.c00c15da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/LoanCalculator/static/js/2.c00c15da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f61d9d685181af93c56e",
+    "url": "/LoanCalculator/static/js/main.4cf439e5.chunk.js"
+  },
+  {
+    "revision": "05aa0db700597097cbbe",
+    "url": "/LoanCalculator/static/js/runtime-main.490314d3.js"
+  }
+]);
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LoanCalculator from './components/LoanCalculator';
-
-ReactDOM.render(
-    <LoanCalculator />,
-    document.getElementById('root')
-);
